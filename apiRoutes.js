cdc47c84b95528c108e1ec92018e76ecdc35e902
Noah <nosherway@yahoo.com>
2021-03-10 22:52:04 -0600

    routes data added

--- apiRoutes.js
+++ apiRoutes.js
@@ -11,9 +11,26 @@ module.exports = (app) => {
 
     //  retreives then adds note to db
     app.post('/api/notes', (req, res) => {
-        
+
+        let lastId;
+        if (noteDB.length) {
+            lastId = Math.max(...(noteDB.map(note => note.id)));
+        } else {
+            lastId = 0;
+        }
+        const id = lastId + 1;
+
+        noteDB.push({ id, ...req.body });
+      
+        res.json(noteDB.slice(-1));
+    });
+
     // note located by ID then deleted
     app.delete('/api/notes/:id', (req, res) => {
-      
+        let getNote = noteDB.find(({ id }) => id === JSON.parse(req.params.id));
+
+        noteDB.splice(noteDB.indexOf(getNote), 1);
+        res.end("deleted!");
     });
-    });
+
+};
